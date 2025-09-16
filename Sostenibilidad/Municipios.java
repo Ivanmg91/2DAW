f38289a4b9c9f13d03de45d01a1ca7faa03c6a57
Ivan <montianogonzalezivan@gmail.com>
2025-09-16 16:05:03 +0200

    update

---
+++ Sostenibilidad/Municipios.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Municipios {
+    public static void main(String[] args) {
+        String pathcsv = "viviendas-por-intensidad-de-uso-a-partir-del-consumo-electrico.-mediana-consumo-anual.csv";
+        String linea;
+        String delimitador = ";";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(pathcsv))) {
+            while ((linea = br.readLine()) != null) {
+                String[] values = linea.split(delimitador);
+                System.out.println(values[3] + ", " + values[4] + ", " + values[2]);
+                System.out.println();
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
+
+/*
+ * Hacer lista de listas y ordenar los municipos por el valor e imprimir las 3
+ * primeras
+ */
--- WebEntornClient/1-1 Uso de variables y .../script.js
+++ WebEntornClient/1-1 Uso de variables y .../script.js
@@ -1,9 +1,9 @@
 const num1 = 1;
 const num2 = 2;
-let num3 = 9;
+let num3 = 2;
 
 let sum = num1 + num1 + num3;
-let bool = Boolean(sum > 10);
+let mayorQ10 = sum > 10;
 
 document.getElementById("num1Display").innerHTML = num1;
 document.getElementById("num2Display").innerHTML = num2;
@@ -11,4 +11,4 @@ document.getElementById("num3Display").innerHTML = num3;
 
 document.getElementById("resultDisplay").innerHTML = sum;
 
-document.getElementById("isOverTen").innerHTML = bool;
+document.getElementById("isOverTen").innerHTML = mayorQ10;
