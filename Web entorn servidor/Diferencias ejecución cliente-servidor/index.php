ba8520a9ec2459bbed8c8d84df4aac4b6de73913
Ivan <montianogonzalezivan@gmail.com>
2025-09-17 20:00:51 +0200

    update

---
+++ Web entorn servidor/Diferencias ejecución cliente-servidor/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Ejemplo Servidor</title>
+</head>
+
+<body>
+    <center>
+        <h1>HTML + PHP</h1>
+        <p>Nombre: Iván</p>
+        <p>Hora: <?php echo date('H:i:s'); ?></p>
+    </center>
+</body>
+
+</html>
