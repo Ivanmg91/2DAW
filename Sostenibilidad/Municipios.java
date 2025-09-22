79d5832fd42412a2fd3fc3079838ee5f8b275752
Iván Montiano González <160036159+Ivanmg91@users.noreply.github.com>
2025-09-22 08:26:05 +0000

    update

--- Sostenibilidad/Municipios.java
+++ Sostenibilidad/Municipios.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Municipios {
     public static void main(String[] args) {
@@ -8,19 +10,35 @@ public class Municipios {
         String linea;
         String delimitador = ";";
 
+        List<String[]> lista = new ArrayList<>();
+
         try (BufferedReader br = new BufferedReader(new FileReader(pathcsv))) {
+
+            br.readLine();
+
             while ((linea = br.readLine()) != null) {
                 String[] values = linea.split(delimitador);
-                System.out.println(values[3] + ", " + values[4] + ", " + values[2]);
-                System.out.println();
+                if (values.length > 4) {
+                    String territorio = values[3].trim();
+                    String valorStr = values[4].trim();
+
+                    try {
+                            double valor = Double.parseDouble(valorStr);
+                            lista.add(new String[]{territorio, valorStr});
+                        } catch (NumberFormatException e) {
+                            
+                        }
+                }
             }
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+
+        lista.sort((a, b) -> Double.compare(Double.parseDouble(b[1]), Double.parseDouble(a[1])));
+        System.out.println("Medianas más altas:");
+        for (int i = 0; i < Math.min(3, lista.size()); i++) {
+            String[] municipio = lista.get(i);
+            System.out.println((i+1) + ". " + municipio[0] + " - " + municipio[1]);
+        }
     }
 }
-
-/*
- * Hacer lista de listas y ordenar los municipos por el valor e imprimir las 3
- * primeras
- */
