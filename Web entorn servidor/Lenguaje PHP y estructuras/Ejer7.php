15cc5c3af59ba877e95234c263e8beac9c46547c
Ivan <montianogonzalezivan@gmail.com>
2025-10-07 17:40:21 +0200

    update

--- Web entorn servidor/Lenguaje PHP y estructuras/Ejer7.php
+++ Web entorn servidor/Lenguaje PHP y estructuras/Ejer7.php
@@ -1,13 +1,48 @@
-<!-- menu_basico -->
+<!-- menu_basico SIN ACABAR-->
 
 <?php
 
-echo "¿Cuantas opciones tendrá el menú?: ";
-$opcionesMenu = rtrim(fgets(STDIN));
+$opcionesMenu = 0;
 $opciones = [];
+$opcionElegida = "";
+$salirMenu = "";
 
-for ($i=0; $i < $opcionesMenu; $i++) { 
-    
+while (!is_numeric($opcionesMenu) || (int)$opcionesMenu == 0) {
+    echo "¿Cuántas opciones tendrá el menú (debe de ser un número)?: ";
+    $opcionesMenu = rtrim(fgets(STDIN));
 }
 
+$opcionesMenu = (int)$opcionesMenu;
+
+echo "Introduce el carácter para terminar el programa: ";
+$salirMenu = rtrim(fgets(STDIN));
+
+for ($i = 0; $i < $opcionesMenu; $i++) {
+    echo "Introduce el caracter para seleccionar la opción del menú " . ($i + 1) . ": ";
+    $opcion = rtrim(fgets(STDIN));
+
+    echo "Introduce el texto de la opción `$opcion`";
+    $texto = rtrim(fgets(STDIN));
+
+    $opciones[$opcion] = $texto;
+}
+
+echo "Este es tu menu: \n";
+foreach ($opciones as $opcion => $texto) {
+    echo "[$opcion] $texto\n";
+}
+
+while ($opcionElegida != "s") {
+    echo "Introduce una opción: ";
+    $opcionElegida = rtrim(fgets(STDIN));
+    
+    for ($i=0; $i < $opciones; $i++) { 
+        if ($opcionElegida == $opciones[$i]) {
+            echo "`Has elegido la opción $opcion[$i]`";
+        }
+    }
+}
+
+
 ?>
+
