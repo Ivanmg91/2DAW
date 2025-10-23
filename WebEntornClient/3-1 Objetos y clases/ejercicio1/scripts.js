e6b57af23f397e74cf47d15cd921b4e1c10b52ad
Ivan <montianogonzalezivan@gmail.com>
2025-10-23 20:23:02 +0200

    update

--- WebEntornClient/3-1 Objetos y clases/ejercicio1/scripts.js
+++ WebEntornClient/3-1 Objetos y clases/ejercicio1/scripts.js
@@ -1,17 +1,50 @@
 const intervalo = {
     minimo: 0,
     maximo: 0,
-}
 
-function esNumeroValido(numero) {
-    while (isNaN(numero)) {
-        alert("Por favor, introduce un número válido.");
-        numero = parseInt(prompt("Por favor introduce un número válido."));
+    get numerosComprendidos() {
+        let comprendidos = [];
+        for (let i = this.minimo; i <= this.maximo; i++) {
+            comprendidos.push(i);
+        }
+        return comprendidos;
+    },
+
+    set maxMin(arrayRandoms) {
+        arrayRandoms.sort((a, b) => a - b);
+        this.minimo = arrayRandoms[0];
+        this.maximo = arrayRandoms[4];
     }
 }
 
-//inicio programa
-intervalo.minimo = prompt("Introduce el mínimo: ");
-intervalo.maximo = prompt("Introduce el máximo: ");
 
-// console.log(intervalo);
+function esMinimoValido() {
+    let min = prompt("Introduce el mínimo: ");
+    while (isNaN(min) || min === '') {
+        min = prompt("Por favor introduce un número válido para el mínimo.");
+    }
+    return parseInt(min);
+}
+
+function esMaximoValido(min) {
+    let max = prompt(`Introduce el máximo: `);
+    while (isNaN(max) || max <= min || max === '') {
+        max = prompt(`Por favor introduce un número válido para el máximo (mayor que ${min}).`);
+    }
+    return parseInt(max);
+}
+
+//inicio programa
+intervalo.minimo = esMinimoValido();
+intervalo.maximo = esMaximoValido(intervalo.minimo);
+
+console.log(`El array según su intervalo es: ${intervalo.numerosComprendidos}\nEl valor mínimo del array es: ${intervalo.minimo}\nEl valor máximo del array es: ${intervalo.maximo}`);
+
+let arrayRandoms = [];
+for (let i = 0; i <= 5; i++) {
+    arrayRandoms.push(Math.floor(Math.random() * 100) + 1)
+}
+
+intervalo.maxMin = arrayRandoms;
+
+console.log(`El array según con número aleatorios es: ${intervalo.numerosComprendidos}\nEl valor mínimo del nuevo array es: ${intervalo.minimo}\nEl valor máximo del nuevo array es: ${intervalo.maximo}`);
