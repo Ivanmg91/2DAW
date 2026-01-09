fd90461c6769fdae3a757bfbbb8f8428d1d84239
Ivan <montianogonzalezivan@gmail.com>
2026-01-09 18:51:35 +0100

    update

--- WebEntornClient/Actividades/5-4 Carrito de Compra/script.js
+++ WebEntornClient/Actividades/5-4 Carrito de Compra/script.js
@@ -1,6 +1,55 @@
 const listaCursos = document.querySelector('#lista-cursos');
-listaCursos.addEventListener('click', extraerDatosCurso);
+listaCursos.addEventListener('click', enviarAlCarrito);
 
-function extraerDatosCurso() {
-    
+function enviarAlCarrito(e) {
+    e.preventDefault();
+
+    const boton = e.target;
+    const card = boton.closest('.card');
+
+    const curso = {
+        // Id del curso
+        id: boton.dataset.id,
+        // id: card.querySelector('a').getAttribute('data-id'),
+
+        imagen: card.querySelector('.imagen-curso').src,
+        nombre: card.querySelector('h4').textContent,
+        precio: card.querySelector('.precio span').textContent,
+        cantidad: 1
+    };
+
+    const existe = existeEnCarrito(curso.id);
+    if (existe) {
+        // Actualizar cantidad
+        actualizarCantidad(curso.id);
+    } else {
+        // Añadirlo nuevo (push)
+        carrito.push(curso);
+    }
+
+    // Refrescar HTML
+    // carritoHTML();          ESTO FALTA
+
+    console.log(curso);
+    console.log(carrito);
+}
+
+// Función para saber si existe en el carrito
+let carrito = [];
+
+function existeEnCarrito(idRecibido) {
+    for (let i = 0; i < carrito.length; i++) {
+        if (carrito[i].id === idRecibido) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function actualizarCantidad(idCurso) {
+    for (let i = 0; i < carrito.length; i++) {
+        if (idCurso === carrito[i].id) {
+            carrito[i].cantidad += 1;
+        }
+    }
 }
